3815c4a7534c0d0b7da9e2067d3ef84c8a0ec6a0
cmnlee <cmnlee@ucdavis.edu>
2019-01-25 03:59:37 -0800

    Cleaned up algorithm logic; Dijkstra seemingly works properly now with both cost functions

--- DijkstraAI.java
+++ DijkstraAI.java
@@ -9,7 +9,7 @@ import java.util.PriorityQueue;
  * This is a sample AI that moves as far horizontally as necessary to reach the target,
  * then as far vertically as necessary to reach the target.  It is intended primarily as
  * a demonstration of the various pieces of the program.
- * 
+ *
  * @author Scott Madera
  */
 public class DijkstraAI implements AIModule {
@@ -23,42 +23,85 @@ public class DijkstraAI implements AIModule {
         // path.add(new Point(currentPoint));
 
         /* we create a PQ of MapNode objects that
-        enables us to order the queue by costs from
-        the StartPoint. A MapNode is a pair <Point, Double>,
-        with the Double value representing the accumulated
-        cost from StartPoint to the Point. */
+         * enables us to order the queue by costs from
+         * the StartPoint. A MapNode is a pair <Point, Double>,
+         * with the Double value representing the accumulated
+         * cost from StartPoint to the Point. */
         PriorityQueue<MapNode> open = new PriorityQueue<>();
-        HashMap<Point, Boolean> closed = new HashMap<Point, Boolean>();
-        HashMap<Point, Point> paths = new HashMap<Point,Point>();
+        HashMap<Point, Boolean> closed = new HashMap<>();
+
+        /* distances will also hold the accumulated costs
+         * from start point to the passed-in point much like open does,
+         * with the passed-in point acting as key, cost acting as value.
+         * We will use this to perform O(1) accesses instead of querying
+         * the PriorityQueue, which can take O(log n) or O(n) operations.
+         *
+         * distances will have the following property:
+         * if some point is found inside distances,
+         * and has a non-null value associated with it,
+         * then that key is inside the frontier. This will
+         * be useful for later cost relaxation purposes. */
+        HashMap<Point, Double> distances = new HashMap<>();
+        HashMap<Point, Point> paths = new HashMap<>(); // format: <current, prev>
+
+        // initialize collections to ensure loop correctness
         open.add(new MapNode(currentPoint, 0.0));
-        Boolean pathVisited = new Boolean(false);
-        while ((map.getEndPoint().x != currentPoint.x) || (map.getEndPoint().y != currentPoint.y)) {
-            // verify if current point has been visited or not.
-            // If so, take node from open list.
-            if (open.isEmpty()) {
-                throw new RuntimeException("Reached empty open list");
-            }
+        distances.put(currentPoint, 0.0);
+        closed.put(currentPoint, true);
+
+        // Boolean pathVisited = new Boolean(false);
+        while (!open.isEmpty()) {
             // set current point as the point associated
             // with the least cost in the frontier set
             currentPoint = open.poll().getPoint();
+            if ((map.getEndPoint().x == currentPoint.x) && (map.getEndPoint().y == currentPoint.y)) {
+                break;
+            }
 
             Point[] neighbors = map.getNeighbors(currentPoint);
-            Point minNeighbor = new Point();
-            Double minCost = Double.MAX_VALUE;
+            // Point minNeighbor = new Point();
+            Double minCost = Double.MAX_VALUE; // make compiler happy
             for (Point neighbor : neighbors) {
                 if (closed.get(neighbor) != null) {
                     continue;
                 }
+
+                /* check if neighbor is in frontier. If distances has a non-null
+                 * value for the given neighbor key, then neighbor is in frontier,
+                 * meaning neighbor already has a cost value tied to it. Set this
+                 * as the minCost. Otherwise set minCost as +infinity.
+                 *
+                 * This is much better than using an open.contains() check
+                 * directly, as checking the PriorityQueue takes O(n) time,
+                 * but querying the distances HashMap takes O(1) time. */
+                boolean frontierFlag = false;
+                if (distances.get(neighbor) != null) {
+                    frontierFlag = true;
+                    minCost = distances.get(neighbor);
+                }
+                else {
+                    minCost = Double.MAX_VALUE;
+                }
                 Double tempCost = map.getCost(currentPoint, neighbor);
-                open.add(new MapNode(neighbor, tempCost)); // adding neighbors to frontier
-                if (tempCost < minCost) {
-                    minCost = tempCost;
-                    minNeighbor = neighbor;
+                if (tempCost + distances.get(currentPoint) < minCost) {
+                    minCost = tempCost + distances.get(currentPoint);
+
+                    /* entering this if statement means that there is
+                     * a better path to the current neighbor, where neighbor
+                     * is inside the frontier. Thus, remove the higher cost path
+                     * of neighbor inside frontier, remove its entry in distances,
+                     * and replace the entries back into the PriorityQueue and HashMap. */
+                    if (frontierFlag) {
+                        MapNode neighborInFrontier = new MapNode(neighbor, distances.get(neighbor));
+                        distances.remove(neighbor); // O(1)?
+                        open.remove(neighborInFrontier); // O(n)
+                    }
+                    distances.put(neighbor, minCost);
+                    paths.put(neighbor, currentPoint);
+                    open.add(new MapNode(neighbor, minCost)); // adding neighbors to frontier; O(log n)
                 }
             }
-
-            closed.put(new Point(currentPoint), true);
-            paths.put(new Point(minNeighbor), new Point(currentPoint));
+            closed.put(currentPoint, true);
         }
 
         // we're doing a bit of extra work here.
@@ -68,10 +111,11 @@ public class DijkstraAI implements AIModule {
         // Are you sure? I looked up the Collections.sort() documentation,
         // and a lot of sources ended up saying it was O(n) anyway, not O(n^2).
         while ((map.getStartPoint().x != currentPoint.x) || (map.getStartPoint().y != currentPoint.y)) {
-            path.add(paths.get(currentPoint));
+            path.add(currentPoint);
             currentPoint = paths.get(currentPoint);
-            System.out.println(currentPoint);
+            // System.out.println(currentPoint);
         }
+        path.add(currentPoint); // don't forget the StartPoint!
         Collections.reverse(path);
 
         // We're done!  Hand it back.
